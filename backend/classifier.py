9178e5937f805bb736a77fc415f3ea8c43822510
이상윤 <speedp001@naver.com>
2023-09-13 00:16:52 +0900

    <backend - item, color, style classifier>
    predict_style 함수 수정 및 mask_img 인자 값 추가

--- backend/classifier.py
+++ backend/classifier.py
@@ -84,7 +84,7 @@ class ItemClassifier:
     #     return reverse_item_dict
     
     def create_style_dictionary(self):
-        style_dict = {'casual': '0', 'dandy': 1, 'formal': '2', 'girlish': '3', 'gorpcore': '4', 'retro': '5', 'romantic': '6', 'sports': '7', 'street': '8'}
+        style_dict = {'casual': '0', 'dandy': '1', 'formal': '2', 'girlish': '3', 'gorpcore': '4', 'retro': '5', 'romantic': '6', 'sports': '7', 'street': '8'}
         
         return style_dict
 
