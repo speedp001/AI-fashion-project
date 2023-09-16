6ad1a9745c132007965d8889fb6d12a46dea2c51
Choi J H <acensia@snu.ac.kr>
2023-09-16 15:06:49 +0900

    Server 내 체크를 위한 print

--- backend/db_search.py
+++ backend/db_search.py
@@ -1,4 +1,4 @@
-####### DB에서 전체 데이터셋 조회 파일
+# DB에서 전체 데이터셋 조회 파일
 
 import numpy as np
 
@@ -6,36 +6,36 @@ from bson.objectid import ObjectId
 from reference import style_array
 
 
-
-####### search code로 DB조회
+# search code로 DB조회
 def DB_search(client, itemcode):
     fashion = client["fashion"]
     item_coll = fashion[str(itemcode[4:6])]
     style = itemcode[1]
     search_code = itemcode[0] + ".*" + itemcode[2:] + "$"
     # print(search_code)
-    singles = item_coll.find({"code":{"$regex": search_code}}, {"_id":0})
-    
+    singles = item_coll.find({"code": {"$regex": search_code}}, {"_id": 0})
+
     matched = []
     styles = np.zeros(9, dtype=int)
-    
+
     for s in singles:
         styles[int(s["code"][1])] += 1
         if s["code"] == itemcode:
             matched.append(s)
-    
+
     rank = np.argsort(styles[:-1])
-    
+
     if len(matched) == 0:
         matched = [style_array[r] for r in rank if styles[r] != 0]
-        print(style_array[rank[0]])
+        if (len(matched)) == 0:
+            print("No data for input")
         return matched, None
     id = matched[0]["root"]
-    sets = fashion["_cloth_sets"].find_one({"_id":ObjectId(id)}, {"_id":0})
+    sets = fashion["_cloth_sets"].find_one({"_id": ObjectId(id)}, {"_id": 0})
     # print(len(sets))
-    print(sets)
+    # print(sets)
     for i in sets["items"]:
         if i["item"] == matched[0]["name"]:
             sets["items"].remove(i)
-    print("success")
+    print("Set is selected")
     return None, sets
--- backend/reference.py
+++ backend/reference.py
@@ -1,56 +1,83 @@
-####### Dictionary 모음
+# Dictionary 모음
 
 # Item dictionary -> DB 참고용
 item_dict = {
     'both_blouse': 0,
-    'both_cardigan': 1, 
-    'both_cargo_pants': 2, 
-    'both_denim_pants': 3, 
-    'both_hooded': 4, 
-    'both_hooded_zipup': 5, 
-    'both_leggings': 6, 
+    'both_cardigan': 1,
+    'both_cargo_pants': 2,
+    'both_denim_pants': 3,
+    'both_hooded': 4,
+    'both_hooded_zipup': 5,
+    'both_leggings': 6,
     'both_long-sleeved_T-shirt': 7,
     'both_long_pants': 8,
     'both_onepiece': 9,
-    'both_shirt': 10, 
-    'both_skirt': 11, 
-    'both_sport_pants': 12, 
-    'both_sweatshirt': 13, 
-    'fw_coat': 14, 
-    'fw_jacket': 15, 
-    'fw_knitwear': 16, 
-    'fw_padding': 17, 
-    'ss_polo_shirt': 18, 
-    'ss_short-sleeved_T-shirt': 19, 
+    'both_shirt': 10,
+    'both_skirt': 11,
+    'both_sport_pants': 12,
+    'both_sweatshirt': 13,
+    'fw_coat': 14,
+    'fw_jacket': 15,
+    'fw_knitwear': 16,
+    'fw_padding': 17,
+    'ss_polo_shirt': 18,
+    'ss_short-sleeved_T-shirt': 19,
     'ss_short_pants': 20
-    }
+}
 
 # Style dictionary
-style_dict = {'casual': '0', 'dandy': '1', 'formal': '2', 'girlish': '3', 'gorpcore': '4', 'retro': '5', 'romantic': '6', 'sports': '7', 'street': '8'}
+style_dict = {'casual': '0', 'dandy': '1', 'formal': '2', 'girlish': '3',
+              'gorpcore': '4', 'retro': '5', 'romantic': '6', 'sports': '7', 'street': '8'}
 
 # Color dictionary
 color_dict = {
-    "beige": '00', 
-    "black": '01', 
-    "blue": '02', 
-    "brown": '03', 
-    "burgundy": '04', 
-    "gray": '05', 
-    "green": '06', 
-    "khaki": '07', 
-    "lightgreen": '08', 
-    "lightpurple": '09', 
-    "mint": '10', 
-    "navy": '11', 
-    "orange": '12', 
-    "pink": '13', 
-    "purple": '14', 
-    "red": '15', 
-    "skyblue": '16', 
-    "teal": '17', 
-    "white": '18', 
+    "beige": '00',
+    "black": '01',
+    "blue": '02',
+    "brown": '03',
+    "burgundy": '04',
+    "gray": '05',
+    "green": '06',
+    "khaki": '07',
+    "lightgreen": '08',
+    "lightpurple": '09',
+    "mint": '10',
+    "navy": '11',
+    "orange": '12',
+    "pink": '13',
+    "purple": '14',
+    "red": '15',
+    "skyblue": '16',
+    "teal": '17',
+    "white": '18',
     "yellow": '19'
 }
 
 # Style DB 조회
-style_array = ['casual', 'dandy', 'formal', 'girlish', 'gorpcore', 'retro', 'romantic', 'sports', 'street']
+style_array = ['casual', 'dandy', 'formal', 'girlish',
+               'gorpcore', 'retro', 'romantic', 'sports', 'street']
+
+
+label_item_dict = {
+    '0':    'both_blouse',
+    '1':    'both_cardigan',
+    '2':    'both_cargo_pants',
+    '3':    'both_denim_pants',
+    '4':    'both_hooded',
+    '5':    'both_hooded_zipup',
+    '6':    'both_leggings',
+    '7':    'both_long-sleeved_T-shirt',
+    '8':    'both_long_pants',
+    '9':    'both_onepiece',
+    '10':     'both_shirt',
+    '11':     'both_skirt',
+    '12':     'both_sport_pants',
+    '13':     'both_sweatshirt',
+    '14':     'fw_coat',
+    '15':     'fw_jacket',
+    '16':     'fw_knitwear',
+    '17':     'fw_padding',
+    '18':     'ss_polo_shirt',
+    '19':     'ss_short-sleeved_T-shirt',
+    '20':    'ss_short_pants'
+}
